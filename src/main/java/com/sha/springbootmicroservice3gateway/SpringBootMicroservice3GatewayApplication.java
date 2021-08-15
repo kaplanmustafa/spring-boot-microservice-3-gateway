ac39b03ad0cea39349876885402e6f9fcf62f1da
INNOVA\muskaplan <muskaplan@innova.com.tr>
2021-08-15 13:13:52 +0300

    project created

---
+++ src/main/java/com/sha/springbootmicroservice3gateway/SpringBootMicroservice3GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sha.springbootmicroservice3gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservice3GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservice3GatewayApplication.class, args);
+	}
+
+}
