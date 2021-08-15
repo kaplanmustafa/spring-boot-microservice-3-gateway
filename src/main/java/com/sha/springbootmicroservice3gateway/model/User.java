88bcd9bb76399aeba56a95b56a44410c787c1a9b
INNOVA\muskaplan <muskaplan@innova.com.tr>
2021-08-15 13:31:58 +0300

    user entity

---
+++ src/main/java/com/sha/springbootmicroservice3gateway/model/User.java
@@ -0,0 +1,28 @@
+package com.sha.springbootmicroservice3gateway.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", unique = true, nullable = false, length = 100)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "create_time")
+    private LocalDateTime createTime;
+}
