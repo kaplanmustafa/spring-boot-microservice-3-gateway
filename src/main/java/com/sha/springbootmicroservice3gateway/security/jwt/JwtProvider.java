ae243e9d7d14c5d0655fad586ba476f1219bc723
INNOVA\muskaplan <muskaplan@innova.com.tr>
2021-08-21 16:21:37 +0300

    jwt methods

---
+++ src/main/java/com/sha/springbootmicroservice3gateway/security/jwt/JwtProvider.java
@@ -0,0 +1,13 @@
+package com.sha.springbootmicroservice3gateway.security.jwt;
+
+import com.sha.springbootmicroservice3gateway.security.UserPrincipal;
+import org.springframework.security.core.Authentication;
+
+import javax.servlet.http.HttpServletRequest;
+
+public interface JwtProvider {
+
+    String generateToken(UserPrincipal authentication);
+
+    Authentication getAuthentication(HttpServletRequest request);
+}
