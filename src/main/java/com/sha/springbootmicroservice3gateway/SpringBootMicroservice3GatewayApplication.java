44f678aef4bddbcecc0fdbf9f0dc3dec9c2854a8
INNOVA\muskaplan <muskaplan@innova.com.tr>
2021-08-28 15:49:45 +0300

    configuration profiles

--- src/main/java/com/sha/springbootmicroservice3gateway/SpringBootMicroservice3GatewayApplication.java
+++ src/main/java/com/sha/springbootmicroservice3gateway/SpringBootMicroservice3GatewayApplication.java
@@ -2,8 +2,10 @@ package com.sha.springbootmicroservice3gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class SpringBootMicroservice3GatewayApplication {
 
     public static void main(String[] args) {
